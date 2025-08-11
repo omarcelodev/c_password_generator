43fb33ae88427d2e70307bcb3c2512799e1edc7b
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-08-11 17:26:57 -0300

    Refatorar gerador de senhas com struct de configuração e melhorias

--- pasword.c
+++ pasword.c
@@ -1,27 +1,44 @@
+//HEADER
 #include <stdio.h>
 #include <time.h>
 #include <locale.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
+#define MIN_SENHA 5
 #define MAX_SENHA 15
+//
 
-//Validar a opcao do usuario
-int validar_opcao_menu(int opcao){
-    if(opcao > 1 || opcao < 0){
-        return true;
-    }else{
-        return false;
-    }
+//STRUCT COM AS CATEGORIAS//
+////////////////////////////
+
+typedef struct {
+    int caracteres;
+    bool maiuscula;
+    bool minuscula;
+    bool numeros;
+    bool simbolos;
+} ConfigSenha;
+
+///////////////////////////
+
+
+//Função para validar a opção selecionada pelo usuário no menu.
+bool validar_opcao_menu(int opcao){
+    return(opcao != 1 && opcao!= 0);
 }
 
-//Capturar opcao do usuario
-int menu(){
+//Função para imprimir o menu.
+void menu(){
+    printf("=== GERADOR DE SENHA ===\n\n");
+    printf("Por favor informe uma opção abaixo.\n");
+}
+
+//Função para capturar a opção do usuário.
+int capturar_opcao_menu(){
     int opcao;
     bool invalida;
-
-    printf("=== GERADOR DE SENHA ===\n\n");
-    printf("Por favor informe uma opção.\n");
 
     do{
         printf("Gerar nova senha (1)\n");
@@ -39,48 +56,45 @@ int menu(){
     return opcao;
 }
 
-//Validar caracteres
+//Função para validar a quantidade de caracteres min e max.
 bool validar_caracteres(int caracteres){
-    if(caracteres < 1 || caracteres > 15){
-        return true;
-    }else{
-        return false;
-    }
+        return (caracteres < MIN_SENHA || caracteres > MAX_SENHA);
 }
 
-//Validar sim ou não
-bool validar_yes_not(int opcao){
-    if(opcao == 'S' || opcao == 's' || opcao == 'N' || opcao == 'n'){
-        return false;
-    } else{
-        return true;
-    }
+//Função para validar sim ou nao.
+bool validar_yes_not(char opcao){
+    return !(opcao == 'S' || opcao == 's' || opcao == 'N' || opcao == 'n');
 }
 
-//Personalização de senha
-void personalizar_senha(){
-    int caracteres;
-    char maiuscula, minuscula, numeros, simbolos, opcao;
+//Função para transformar a resposta do usuário em um booleano.
+bool sim_ou_nao(char opcao) {
+    return (opcao == 'S' || opcao == 's');
+}
+
+//Função para o usuario personalizar a senha.
+void personalizar_senha(ConfigSenha *config){
+    char opcao;
     bool invalida;
 
     printf("\nEste programa irá gerar uma senha segura para você.\n");
     printf("Basta informar as preferências abaixo.\n\n");
 
+//Quantidade de Caracteres.
     do{
         printf("1) Quantos caracteres a senha deve ter? ");
-        scanf("%d", &caracteres);
-        invalida = validar_caracteres(caracteres);
+        scanf("%d", &config->caracteres);
+        invalida = validar_caracteres(config->caracteres);
 
         if(invalida){
-            printf("O número de caracteres deve ser maior que 0 e menor que 16\n\n");
+            printf("O número de caracteres deve ser maior que 4 e menor que 16\n\n");
         }
 
     }while(invalida);
 
+//Incluir Maiusculas.
     do{
         printf("2) Incluir letras maiúsculas? (S/N): ");
-        scanf(" %c", &maiuscula);
-        opcao = maiuscula;
+        scanf(" %c", &opcao);
         invalida = validar_yes_not(opcao);
 
         if(invalida){
@@ -88,11 +102,12 @@ void personalizar_senha(){
         }
 
     }while(invalida);
+    config->maiuscula = sim_ou_nao(opcao);
 
+//Incluir Minusculas.
     do{
         printf("3) Incluir letras minúsculas? (S/N): ");
-        scanf(" %c", &minuscula);
-        opcao = minuscula;
+        scanf(" %c", &opcao);
         invalida = validar_yes_not(opcao);
 
         if(invalida){
@@ -100,11 +115,12 @@ void personalizar_senha(){
         }
 
     }while(invalida);
+    config->minuscula = sim_ou_nao(opcao);
 
+//Incluir Números.
     do{
         printf("4) Incluir números? (S/N): ");
-        scanf(" %c", &numeros);
-        opcao = numeros;
+        scanf(" %c", &opcao);
         invalida = validar_yes_not(opcao);
 
         if(invalida){
@@ -112,11 +128,12 @@ void personalizar_senha(){
         }
 
     }while(invalida);
+    config->numeros = sim_ou_nao(opcao);
 
+//Incluir Simbolos.
     do{
         printf("5) Incluir símbolos especiais? (S/N): ");
-        scanf(" %c", &simbolos);
-        opcao = simbolos;
+        scanf(" %c", &opcao);
         invalida = validar_yes_not(opcao);
 
         if(invalida){
@@ -124,13 +141,57 @@ void personalizar_senha(){
         }
 
     }while(invalida);
+    config->simbolos = sim_ou_nao(opcao);
 }
 
-//Executar a opcao do usuario
+//Função para gerar a senha.
+void gerar_senha(ConfigSenha *config){
+    char senha[MAX_SENHA + 1] = {0};
+    char pool[100] = {0};
+    int pool_len = 0;
+
+    if(config->maiuscula){
+        strcat(pool, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+
+    if(config->minuscula){
+        strcat(pool, "abcdefghijklmnopqrstuvwxyz");
+    }
+
+    if(config->numeros){
+        strcat(pool, "0123456789");
+    }
+
+    if(config->simbolos){
+        strcat(pool, "!@#$%^&*()-_=+[]{}|;:',.<>/?");
+    }
+
+    pool_len = strlen(pool);
+
+    if(pool_len == 0){
+        printf("Erro: Nenhum tipo de caractere selecionado para gerar a senha.\n");
+        return;
+    }
+
+    srand(time(NULL));
+
+    for(int i = 0; i < config->caracteres; i++){
+        int pos = rand() % pool_len;
+        senha[i] = pool[pos];
+    }
+
+    senha[config->caracteres] = '\0';
+
+    printf("\nSenha gerada: %s\n", senha);
+
+}
+
+//Executar a opcao selecionada pelo usuario.
 void executar_opcao(int opcao){
     if(opcao == 1){
-        personalizar_senha();
-        gerar_senha();
+        ConfigSenha config;
+        personalizar_senha(&config);
+        gerar_senha(&config);
     }
     else{
         printf("Programa Encerrado.\n");
@@ -138,20 +199,17 @@ void executar_opcao(int opcao){
 
 }
 
-//Gerar senhar
-void gerar_senha(){
-    char senha[MAX_SENHA + 1] = {0};
-
-
-}
-
-
-//MAIN
+//Função Principal.
 int main(){
     setlocale(LC_ALL, "Portuguese");
     system("color 0A");
+    int opcao;
 
-    int opcao = menu();
-    executar_opcao(opcao);
+    do{
+        menu();
+        opcao = capturar_opcao_menu();
+        executar_opcao(opcao);
+    }while(opcao != 0);
+
     return 0;
 }
